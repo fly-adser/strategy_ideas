59bd85705f27300f58d25b7cf63b464b1364dab2
刘垚磊(Liu Yaolei) <80354734@adc.com>
2022-04-11 19:51:06 +0800

    'classInfo'

---
+++ mix_ranker/envs/ClassInfo.py
@@ -0,0 +1,42 @@
+
+class AdinfoReq(object):
+
+    def __init__(self, adId=None, ecpm=None, fixedPos=None):
+        self.adId     = adId
+        self.ecpm     = ecpm
+        self.fixedPos = fixedPos
+
+class ContentInfoReq(object):
+
+    def __init__(self, contentId=None, score=None, fixedPos=None):
+        self.contentId = contentId
+        self.score     = score
+        self.fixedPod  = fixedPos
+
+class AdinfoRes(object):
+
+    def __init__(self, adId=None, ecpm=None, pos=None):
+        self.adId = adId
+        self.ecpm = ecpm
+        self.pos  = pos
+
+class ContentInfoRes(object):
+
+    def __init__(self, contendId=None, score=None, pos=None):
+        self.contendId = contendId
+        self.score     = score
+        self.pos       = pos
+
+class MixRankerRequest(object):
+
+    def __init__(self, requestId=None, pages=None, adInfoReqs=[], contentInfoReqs=[]):
+        self.requestId       = requestId
+        self.pages           = pages
+        self.adInfoReqs      = adInfoReqs
+        self.contentInfoReqs = contentInfoReqs
+
+class MixRankerResponse(object):
+
+    def __init__(self, adInfoReqs=[], contentInfoReqs=[]):
+        self.adInfoReqs = adInfoReqs
+        self.contentInfoReqs = contentInfoReqs
