316e2ffb39bfcc3f5d1042b8750f623e53528a93
刘垚磊(Liu Yaolei) <80354734@adc.com>
2022-04-18 19:22:09 +0800

    'fixEstimate'

--- mix_ranker/script/mix_common/Estimate.py
+++ mix_ranker/script/mix_common/Estimate.py
@@ -14,7 +14,7 @@ class Estimate(object):
         if not listAds or len(listAds)<=0: return
 
         for adinfoRes in listAds:
-            self.cumulateAds += adinfoRes.ecpm / (self.c * math.log2(adinfoRes.pos+1))
+            self.cumulateAds += self.c * adinfoRes.ecpm / (math.log2(adinfoRes.pos+1))
             self.adPos.append(adinfoRes.pos)
 
     def computeDCGForNews(self, listNews):
@@ -22,5 +22,5 @@ class Estimate(object):
         if not listNews or len(listNews)<=0: return
 
         for contentInfoRes in listNews:
-            self.cumulateNews += contentInfoRes.score / (self.c * math.log2(contentInfoRes.pos+1))
+            self.cumulateNews += self.c * contentInfoRes.score / (math.log2(contentInfoRes.pos+1))
             self.newPos.append(contentInfoRes.pos)
